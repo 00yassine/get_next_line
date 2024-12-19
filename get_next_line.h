420df11963995137a1efdd26f062ef0ec768c8a7
Yassine <yassineparis26@gmain.com>
2024-12-20 00:37:48 +0100

    get next line

---
+++ get_next_line.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yassine <yassine@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/19 22:49:01 by yassine           #+#    #+#             */
+/*   Updated: 2024/12/19 22:50:18 by yassine          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1234
+# endif
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+int		ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*read_first_line(int fd, char *text);
+char	*get_the_line(char *text);
+char	*clean_first_line(char *text);
+void	*ft_calloc(unsigned int count, unsigned int size);
+
+#endif
